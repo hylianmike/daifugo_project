c6268a90211b30bf8d40b9ea61e8292e2b5aed7a
Jack <jackdouma0@gmail.com>
2024-10-31 12:49:41 -0400

    added game scene

---
+++ src/main/kotlin/com/example/daifugo/GameApplication.kt
@@ -0,0 +1,21 @@
+package com.example.daifugo
+
+import javafx.application.Application
+import javafx.fxml.FXMLLoader
+import javafx.scene.Scene
+import javafx.stage.Stage
+
+class GameApplication : Application()
+{
+    override fun start(stage: Stage) {
+        val fxmlLoader = FXMLLoader(GameApplication::class.java.getResource("game.fxml"))
+        val scene = Scene(fxmlLoader.load())
+        stage.title = "Daifugo!"
+        stage.scene = scene
+        stage.show()
+    }
+}
+
+fun main() {
+    Application.launch(GameApplication::class.java)
+}
---
+++ src/main/kotlin/com/example/daifugo/GameController.kt
@@ -0,0 +1,33 @@
+package com.example.daifugo
+
+import javafx.fxml.FXML
+import javafx.scene.control.Button
+import javafx.scene.control.Label
+
+class GameController {
+
+    @FXML
+    private lateinit var fourOfAKindButton: Button
+
+    @FXML
+    private lateinit var passButton: Button
+
+    @FXML
+    private lateinit var playButton: Button
+
+    @FXML
+    private lateinit var singlesButton: Button
+
+    @FXML
+    private lateinit var straightsButton: Button
+
+    @FXML
+    private lateinit var threeOfAKindButton: Button
+
+    @FXML
+    private lateinit var turnLabel: Label
+
+    @FXML
+    private lateinit var twoOfAKindButton: Button
+
+}
