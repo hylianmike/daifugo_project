280a974cafc396647b60443427e89663a640d23c
LiamSlappendel <97615261+LiamSlappendel@users.noreply.github.com>
2024-11-23 08:22:13 -0500

    A bunch of changes including initial LAN functionality implemented(not working yet)

--- src/main/kotlin/com/example/daifugo/HomeController.kt
+++ src/main/kotlin/com/example/daifugo/HomeController.kt
@@ -13,8 +13,15 @@ import javafx.scene.control.Label
 import javafx.scene.control.TextField
 import javafx.stage.Stage
 import java.io.IOException
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
 import java.net.URL
 import java.util.*
+import java.net.ServerSocket
+import java.net.Socket
+import java.net.DatagramSocket
+import java.net.DatagramPacket
+import java.net.InetAddress
 import kotlin.concurrent.thread
 
 class HomeController: Initializable
