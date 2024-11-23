fd62c3cb617368af4e974c955a4b5e8c7a11193c
LiamSlappendel <97615261+LiamSlappendel@users.noreply.github.com>
2024-11-23 10:06:49 -0500

    Reverted LAN changes due to an issue(prior changes are on my device seperately)

--- src/main/kotlin/com/example/daifugo/HomeController.kt
+++ src/main/kotlin/com/example/daifugo/HomeController.kt
@@ -13,15 +13,8 @@ import javafx.scene.control.Label
 import javafx.scene.control.TextField
 import javafx.stage.Stage
 import java.io.IOException
-import java.io.ObjectInputStream
-import java.io.ObjectOutputStream
 import java.net.URL
 import java.util.*
-import java.net.ServerSocket
-import java.net.Socket
-import java.net.DatagramSocket
-import java.net.DatagramPacket
-import java.net.InetAddress
 import kotlin.concurrent.thread
 
 class HomeController: Initializable
@@ -89,104 +82,33 @@ class HomeController: Initializable
     private val host = ""  // SET TO THE HOST IP
     private var server: Server? = null
     private var client: Client? = null
-    private var players = 0
-    private var hosting = false
-    private var serverSocket: ServerSocket? = null
-    private var gameStarted = false
-    private var clientSockets = mutableListOf<Socket>()
+    private var players = 1
 
-    /*********************** HOSTING A LOBBY ************************/
+    /**
+     * On press, lobby is created
+     */
     @FXML
     fun lanHostLobbyButtonPress(event: ActionEvent)
     {
-        if(!hosting) {
-            println("Hosting lobby at $host:$port")
+        //hide the playgameButton
+        playGameButton.isVisible = false
+        lanJoinLobbyButton.isVisible = false
 
-            try {
-                // make server
-                serverSocket = ServerSocket(port)
-                errorLabel.text = "Server started on $host:$port"
-                println("Server started on $host:$port")
-                updatePlayersCount()
-                //acceptClients()
-            } catch (e: IOException) {
-                errorLabel.text = "Error starting server: ${e.message}"
-                println("Error starting server: ${e.message}")
-            }
-            //broadcastServer()
+        println("Hosting lobby at $host:$port")
 
-            //hide the playgameButton
-            playGameButton.isVisible = false
-            lanJoinLobbyButton.isVisible = false
-
-            //change the text of the host lobby button to "cancel hosting"
-            lanHostLobbyButton.text = "Cancel Hosting"
-
+        try {
+            // make server
+            server = Server(port, this)
+            server?.start()
+            errorLabel.text = "Server started on $host:$port"
+            println("Server started on $host:$port")
+            lanPlayersLabel.text = "1/4"
         }
-        else{
-            try{
-                //server.close()
-                //close the server socket
-            }
-            catch(e: IOException){
-                errorLabel.text = "Error closing server: ${e.message}"
-                println("Error closing server: ${e.message}")
-            }
-            //change the text of the host lobby button to "host lobby"
-            lanHostLobbyButton.text = "Host Lobby"
-            //show the playgameButton
-            playGameButton.isVisible = true
-            lanJoinLobbyButton.isVisible = true
-        }
-        hosting = !hosting
-    }
-
-
-
-    /**
-     * Pressed when the host wants to start the game
-     */
-    @FXML
-    fun lanStartGameButtonPress(event: ActionEvent)
-    {
-        gameStarted = true
-        println("Starting game...")
-        //broadcast to all clients that the game has started
-        clientSockets.forEach { socket ->
-            try {
-                val outputStream = ObjectOutputStream(socket.getOutputStream())
-                outputStream.writeObject("start")
-                outputStream.flush()
-            } catch (e: IOException) {
-                println("Error sending start message: ${e.message}")
-            }
+        catch (e: IOException) {
+            errorLabel.text = "Error starting server: ${e.message}"
+            println("Error starting server: ${e.message}")
         }
     }
-
-    private fun acceptClients() {
-        println("Accepting clients...")
-        while (!gameStarted){
-            val clientSocket = serverSocket?.accept()
-            println("Client connected: ${clientSocket?.remoteSocketAddress}")
-            updatePlayersCount()
-            broadcastPlayerCount()
-        }
-    }
-
-    private fun broadcastPlayerCount() {
-        clientSockets.forEach { socket ->
-            try {
-                val outputStream = ObjectOutputStream(socket.getOutputStream())
-                outputStream.writeObject(players)
-                outputStream.flush()
-            } catch (e: IOException) {
-                println("Error sending player count: ${e.message}")
-            }
-        }
-        updatePlayersCount()
-    }
-
-    /************************** JOINING A GAME **************************/
 
     /**
      * On press, user joins lobby
@@ -199,8 +121,8 @@ class HomeController: Initializable
 
         try {
             // initialize client and connect to host
-            val socket = Socket(host, port)
-
+            client = Client(host, port, this)
+            client?.connect()
             println("Connected to server at $host:$port")
             lanPlayersLabel.text = "${players}/4"
         }
@@ -210,55 +132,25 @@ class HomeController: Initializable
         }
     }
 
-    fun clientSideConnection() {
-        val socket = Socket(host, port)
-        val inputStream = ObjectInputStream(socket.getInputStream())
-        val players = inputStream.readObject() as Int
-        println("Received player count: $players")
+    /**
+     * Pressed when the host wants to start the game
+     */
+    @FXML
+    fun lanStartGameButtonPress(event: ActionEvent)
+    {
+
     }
 
-    /***************************** OTHER *******************************/
     /**
      * Update player count
      */
     fun updatePlayersCount()
     {
         players++
-        println("Players: $players")
 
         // Use Platform.runLater to update the UI on the JavaFX thread
         Platform.runLater {
             lanPlayersLabel.text = "${players}/4"
-        }
-    }
-
-    fun broadcastServer() {
-        val localHost = InetAddress.getLocalHost()
-        println("Local host: $localHost")
-
-        val socket = DatagramSocket()
-        val message = "GameServer"
-        val packet = DatagramPacket(
-            message.toByteArray(),
-            message.length,
-            InetAddress.getByName("255.255.255.255"),
-            port
-        )
-        socket.send(packet)
-        println("Broadcasted on ")
-        socket.close()
-    }
-
-    fun listenForBroadcast() {
-        val socket = DatagramSocket(port)  // Listen on the same port used for broadcasting
-        val buffer = ByteArray(1024)  // Buffer to store incoming messages
-        val packet = DatagramPacket(buffer, buffer.size)
-
-        println("Listening for broadcasts on port 8888...")
-        while (true) {
-            socket.receive(packet)  // Wait for a broadcast message
-            val message = String(packet.data, 0, packet.length)  // Extract the message
-            println("Received message: '$message' from ${packet.address.hostAddress}")
         }
     }
 }
